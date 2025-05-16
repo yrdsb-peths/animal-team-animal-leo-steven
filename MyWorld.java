dea4a9343c0a8b59b36da16e845a6f9ec348fdcf
LeooSo <sol542939@gmail.com>
2025-05-16 14:28:10 -0400

    Revert "added apple class"
    This reverts commit 5796ef2ca0971de2dd068569a6acbadcf210e050.

--- MyWorld.java
+++ MyWorld.java
@@ -1,26 +1,8 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    public MyWorld() 
-    {
+    public MyWorld() {
         super(600, 400, 1);
-    }
-    
-    /**
-     * Game over
-     */
-    public void gameOver()
-    {
-        Label gameOverLabel = new Label("Game over", 100);
-        addObject(gameOverLabel, 300, 200);
-    }
-
-    public void createApple()
-    {
-        Apple apple = new Apple();
-        int x = Greenfoot.getRandomNumber(600);
-        int y = 0;
-        addObject(apple, x, y);        
     }
 }
 //comment
